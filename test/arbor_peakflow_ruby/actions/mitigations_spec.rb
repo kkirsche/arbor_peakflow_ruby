784225365485e0e2261bf52b9be2035a4098abf0
Kevin Kirsche <kevin.kirsche@verizon.com>
2015-03-27 09:20:03 -0400

    Mitigations testing

--- test/arbor_peakflow_ruby/actions/mitigations_spec.rb
+++ test/arbor_peakflow_ruby/actions/mitigations_spec.rb
@@ -3,8 +3,8 @@ require 'minitest_helper'
 module ArborPeakflowTest
   # Test the alerts actions
   class ArborPeakflowClient
-    describe 'Managed Object', 'The Arbor Web Services API Managed Object options' do
-      it 'should retrieve JSON Managed Object data' do
+    describe 'Mitigations', 'The Arbor Web Services API mitigation options' do
+      it 'should retrieve JSON Mitigations data' do
         @mock_network = Minitest::Mock.new
         @mock_network.expect(:get, id: '123456')
 
@@ -15,22 +15,24 @@ module ArborPeakflowTest
         Faraday.stub :new, @mock_network, @mock_request do
           client = Arbor::Peakflow::Client.new host: 'http://test.host.com',
                                                api_key: 'myAPIKey'
-          response = client.managed_object 'filter'
+          response = client.mitigations
           response.wont_be_nil
         end
       end
 
-      it 'should retrieve JSON managed object options info with parameters' do
+      it 'should retrieve JSON mitigations options info with parameters' do
         @mock_request = Minitest::Mock.new
         @mock_request.expect(:url, { url: 'arborws/admin/managed_object' },
                              [String])
         @mock_request.expect(:params, api_key: 'myAPIKey')
         @mock_request.expect(:params, filter: 'filter')
+        @mock_request.expect(:params, limit: 100)
+        @mock_request.expect(:params, format: 'json')
 
         client = Arbor::Peakflow::Client.new host: 'http://test.host.com',
                                              api_key: 'myAPIKey'
         client.conn.stub :get, nil, @mock_request do
-          client.cp5500 'filter'
+          client.mitigations 'filter', 100, 'json'
           @mock_request.verify
         end
       end
