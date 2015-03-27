1199390df29215378c8d57aa25dacd9ba130f1ca
Kevin Kirsche <kevin.kirsche@verizon.com>
2015-03-27 08:31:29 -0400

    Improve tests

---
+++ test/arbor_peakflow_ruby/actions/cp5500_spec.rb
@@ -0,0 +1,24 @@
+require 'minitest_helper'
+
+module ArborPeakflowTest
+  # Test the alerts actions
+  class ArborPeakflowClient
+    describe 'CP5500', 'The Arbor Web Services API CP5500 appliance settings' do
+      it 'should retrieve JSON CP5500 info' do
+        @mock_network = Minitest::Mock.new
+        @mock_network.expect(:get, id: '123456')
+
+        @mock_request = Minitest::Mock.new
+        @mock_request.expect(:request, true, [Symbol])
+        @mock_request.expect(:adapter, true, [Symbol])
+
+        Faraday.stub :new, @mock_network, @mock_request do
+          client = Arbor::Peakflow::Client.new host: 'http://test.host.com',
+                                               api_key: 'myAPIKey'
+          response = client.cp5500 'action', 'filter'
+          response.wont_be_nil
+        end
+      end
+    end
+  end
+end
--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -1,7 +1,7 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'arbor_peakflow_ruby'
 require 'codeclimate-test-reporter'
 CodeClimate::TestReporter.start
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'arbor_peakflow_ruby'
 require 'minitest/autorun'
 
 # ElasticsearchUpdate houses all tests for the
